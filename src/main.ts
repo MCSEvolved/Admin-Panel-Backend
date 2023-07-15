3e775aef1816ff7d0a2caa98a0fded7eab228b79
IICarst <zwartenkotcarst@gmail.com>
2023-07-15 16:59:59 +0200

    added claims guard

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ClaimsGuard } from './shared/claims.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: {origin: '*'}});
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   app.setGlobalPrefix('admin-panel')
+  app.useGlobalGuards(new ClaimsGuard(['isPlayer']))
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/shared/claims.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
+
+interface ClaimsRo {
+    email: string,
+    role: string
+}
+
+@Injectable()
+export class ClaimsGuard implements CanActivate {
+    constructor(private roles: string[]) {}
+
+    async canActivate(
+        context: ExecutionContext,
+    ): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        try {
+            const claims = await axios.get<ClaimsRo>("https://api.mcsynergy.nl/auth/get-user-claims", {
+                headers: {Authorization: request.headers.authorization}
+            })
+            console.log(claims.data)
+            return this.roles.includes(claims.data.role);
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.UNAUTHORIZED)
+        }
+    }
+}
