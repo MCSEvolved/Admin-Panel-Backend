f16468282023b4ff8b2e3cf8529cd85deaa8644c
IICarst <zwartenkotcarst@gmail.com>
2023-10-06 10:27:26 +0200

    added more logging

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { NestFactory } from '@nestjs/core';
+import {HttpAdapterHost, NestFactory} from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ClaimsGuard } from './shared/claims.guard';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import {AllExceptionsFilter} from "./shared/exception-filter";
+import {LoggingInterceptor} from "./shared/logging-interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: { origin: '*' }, logger: ['log', 'error', 'warn', 'debug', 'verbose'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('admin-panel');
   app.useGlobalGuards(new ClaimsGuard(['isAdmin']));
+  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors({
     origin: [
       /http\:\/\/localhost\:\d+/,
---
+++ src/shared/exception-filter.ts
@@ -0,0 +1,28 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus, Logger,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+
+    const logger = new Logger()
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    logger.error(`[${httpStatus}] ${exception['message']}`)
+
+    httpAdapter.reply(ctx.getResponse(), exception['response'], httpStatus);
+  }
+}
